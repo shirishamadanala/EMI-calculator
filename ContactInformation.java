930b02a80dddbc6524a3e51a376f3613d6c7469c
shirishamadanala <34132378+shirishamadanala@users.noreply.github.com>
2018-03-13 13:52:42 +0530

    Add files via upload

---
+++ ContactInformation.java
@@ -0,0 +1,119 @@
+package com.developer.smadanala.easyemi;
+
+
+import android.content.Intent;
+
+import android.net.Uri;
+
+import android.support.v7.app.ActionBarActivity;
+
+import android.os.Bundle;
+
+import android.view.Menu;
+
+import android.view.MenuItem;
+
+
+
+
+public class ContactInformation extends ActionBarActivity
+ {
+
+   
+ @Override
+  
+  protected void onCreate(Bundle savedInstanceState)
+ {
+      
+  super.onCreate(savedInstanceState);
+   
+     setContentView(R.layout.activity_contact_information);
+  
+      getSupportActionBar().setHomeButtonEnabled(true);
+     
+   getSupportActionBar().setDisplayShowHomeEnabled(true);
+     
+   getSupportActionBar().setDisplayUseLogoEnabled(true);
+      
+  getSupportActionBar().setDisplayShowTitleEnabled(true);
+     
+   getSupportActionBar().setIcon(R.drawable.easy_emi_launcher);
+
+    }
+
+   
+ @Override
+ 
+   public boolean onCreateOptionsMenu(Menu menu)
+ {
+    
+    // Inflate the menu; this adds items to the action bar if it is present.
+     
+   getMenuInflater().inflate(R.menu.menu_contact_information, menu);
+    
+    return true;
+   
+ }
+
+  
+  @Override
+   
+ public boolean onOptionsItemSelected(MenuItem item)
+ {
+       
+ // Handle action bar item clicks here. The action bar will
+ 
+       // automatically handle clicks on the Home/Up button, so long
+    
+    // as you specify a parent activity in AndroidManifest.xml.
+     
+   int id = item.getItemId();
+
+ 
+       switch (id)
+ {
+        
+    case R.id.action_contact:
+                Intent contactIntent = new Intent(this, ContactInformation.class);
+            
+    startActivity(contactIntent);
+             
+   break;
+     
+       case R.id.action_share:
+     
+           Intent sendIntent = new Intent();
+      
+          sendIntent.setAction(Intent.ACTION_SEND);
+  
+
+              sendIntent.putExtra(Intent.EXTRA_TEXT, Constants.SHARE_INFORMATION);
+   
+             sendIntent.setType("text/plain");
+             
+   startActivity(sendIntent);
+             
+   break;
+          
+  case R.id.action_rate:
+    
+            Intent rateIntent = new Intent();
+  
+              rateIntent.setData(Uri.parse(Constants.MOBILE_APP_LINK));
+ 
+               startActivity(rateIntent);
+               
+ break;
+      
+      default:
+                return super.onOptionsItemSelected(item);
+     
+   }
+
+     
+   return super.onOptionsItemSelected(item);
+  
+  }
+
+}
+
---
+++ YearsStatistics.java
@@ -0,0 +1,152 @@
+package com.developer.smadanala.easyemi;
+
+
+import android.app.Activity;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.Gravity;
+
+import android.widget.TableLayout;
+
+import android.widget.TableRow;
+
+import android.widget.TextView;
+
+
+import java.text.DecimalFormat;
+
+import java.text.NumberFormat;
+
+import java.util.Locale;
+
+
+public class YearsStatistics extends Activity
+ {
+
+   
+	 @Override
+   
+	 protected void onCreate(Bundle savedInstanceState) 
+	{
+       
+	 super.onCreate(savedInstanceState);
+ 
+       setContentView(R.layout.activity_years_statistics);
+
+        Intent intent = getIntent();
+    
+       long totalAmt = intent.getLongExtra(MainActivity.TOTAL_AMOUNT, 0l);
+  
+      double interest = intent.getDoubleExtra(MainActivity.INTEREST, 0.0);
+ 
+       double emi = intent.getDoubleExtra(MainActivity.EMI, 0.0);
+  
+      NumberFormat formatter = DecimalFormat.getInstance(Locale.getDefault());
+
+ 
+       TableLayout tableLayout = (TableLayout) findViewById(R.id.statistics_years_table);
+
+    
+       double principalPaid = 0;
+
+        double principalPaidSoFar = 0;
+ 
+       double interestPaid = 0;
+     
+        double interestPaidSoFar = 0;
+        
+       int i = 0;
+ 
+       int j = 0;
+ 
+       while (Math.round(totalAmtLeft) > 0) 
+       {
+    
+        i++;
+   
+         interestPaid = interest * totalAmtLeft;
+
+            interestPaidSoFar += interestPaid;
+  
+          principalPaid = emi - interestPaid;
+   
+         principalPaidSoFar += principalPaid;
+         
+         totalAmtLeft = totalAmtLeft - principalPaid;
+
+      
+         if ((i % 12 == 0)
+       || (Math.round(totalAmtLeft) <= 0)) 
+        {
+        
+               j++;
+ 
+               TableRow tableRow = new TableRow(this);
+
+ 
+               TextView indexView = new TextView(this);
+ 
+               indexView.setText(j + "");
+             
+               indexView.setGravity(Gravity.CENTER);
+               
+               TableRow.LayoutParams params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 1f);
+       
+               indexView.setLayoutParams(params);
+ 
+               tableRow.addView(indexView);
+
+   
+               TextView principalPaidView = new TextView(this);
+ 
+               principalPaidView.setText(formatter.format(Math.round(principalPaidSoFar)) + "");
+              
+              principalPaidView.setGravity(Gravity.CENTER);
+ 
+               params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 3f);
+ 
+               principalPaidView.setLayoutParams(params);
+           
+              tableRow.addView(principalPaidView);
+
+              
+              TextView interestPaidView = new TextView(this);
+             
+           interestPaidView.setText(formatter.format(Math.round(interestPaidSoFar)) + "");
+   
+             interestPaidView.setGravity(Gravity.CENTER);
+               
+             params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 3f);
+              
+           interestPaidView.setLayoutParams(params);
+    
+            tableRow.addView(interestPaidView);
+
+           
+           TextView totalAmtLeftView = new TextView(this);
+      
+          totalAmtLeftView.setText(formatter.format(Math.round(totalAmtLeft)) + "");
+       
+         totalAmtLeftView.setGravity(Gravity.CENTER);
+              
+         params = new TableRow.LayoutParams(TableRow.LayoutParams.WRAP_CONTENT, TableRow.LayoutParams.WRAP_CONTENT, 3f);
+ 
+               totalAmtLeftView.setLayoutParams(params);
+               
+           tableRow.addView(totalAmtLeftView);
+
+               
+         tableLayout.addView(tableRow);
+        
+              interestPaidSoFar = 0;
+             
+              principalPaidSoFar = 0;
+           
+ }
+       
+ }
+   
+ }
+
+}
